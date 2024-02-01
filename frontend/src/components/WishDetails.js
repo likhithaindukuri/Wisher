a6505a3c5b76c3afea62ee27339c9aa8a599cb7b
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2024-02-01 22:21:45 +0530

    Installed some packages and added confetti to the wish card

--- frontend/src/components/WishDetails.js
+++ frontend/src/components/WishDetails.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import  { useState } from 'react';
 import { useWishesContext } from '../hooks/useWishesContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import Confetti from 'react-confetti';
 
 const WishDetails = ({ wish,email }) => {
   const { dispatch } = useWishesContext()
   const { user } = useAuthContext()
 
+  const [confettiPosition, setConfettiPosition] = useState({ x: 0, y: 0 });
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  const handleMouseMove = (e) => {
+    if (isMouseOver) {
+      const rect = e.currentTarget.getBoundingClientRect();
+      setConfettiPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    }
+  };
+
+  const handleMouseEnter = () => {
+    setMouseOver(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseOver(false);
+  };
   const handleClick = async () => {
     if (!user) {
       return
@@ -28,7 +47,17 @@ const WishDetails = ({ wish,email }) => {
 
   return (
     <div className="home">
-      <div className="wish-details">
+      <div className="wish-details" onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {isMouseOver && (
+          <Confetti
+            numberOfPieces={50}
+            gravity={0.2}
+            wind={0.1}
+            width={window.innerWidth}
+            height={window.innerHeight}
+            confettiSource={confettiPosition}
+          />
+        )}
         <h4>{wish.title}</h4>
         <p><strong>Text: </strong>{wish.text}</p>
         <p><strong>Date: </strong>{new Date(wish.date).toLocaleDateString('en-GB')}</p>
